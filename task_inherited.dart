bf4f1a2c6a4ad545d4df051041d077273bde5647
Ana Flavia Lobato <22400656@aluno.cotemig.com.br>
2025-05-07 15:03:53 -0300

    Update task_inherited.dart

--- task_inherited.dart
+++ task_inherited.dart
@@ -7,23 +7,23 @@ class TaskInherited extends InheritedWidget {
     required Widget child,
   }) : super(key: key, child: child);
 
-  final List<Tasks> taskList = [
-      Tasks('Estudar Flutter', 'assets/images/flutter.png', 3),
-      Tasks('Andar de Bike', 'assets/images/bike.webp', 2),
-      Tasks('Ler 50 páginas', 'assets/images/ler.jpg', 1),
-      Tasks('Meditar', 'assets/images/meditar.jpeg', 4),
-      Tasks('Jogar','assets/images/jogar.jpg',0, ),
-      
+  final List<Task> taskList = [
+    Task('Aprender Flutter', 'assets/images/dash.png', 3),
+    Task('Andar de Bike', 'assets/images/bike.webp', 2),
+    Task('Meditar', 'assets/images/meditar.jpeg', 5),
+    Task('Ler', 'assets/images/livro.jpg', 4),
+    Task('Jogar', 'assets/images/jogar.jpg', 1),
   ];
 
-  void newTask(String name, String photo, int difficulty ){
-    taskList.add(Tasks(name, photo, difficulty));
-
+  void newTask(String name, String photo,int difficulty){
+    taskList.add(Task(name, photo, difficulty));
   }
 
-
-  static TaskInherited? of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<TaskInherited>();
+  static TaskInherited of(BuildContext context) {
+    final TaskInherited? result =
+        context.dependOnInheritedWidgetOfExactType<TaskInherited>();
+    assert(result != null, 'No TaskInherited found in context');
+    return result!;
   }
 
   @override
